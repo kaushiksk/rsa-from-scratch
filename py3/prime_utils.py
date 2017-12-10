1ab600f0c7937f05b83c74d491db744f16235a85
kaushiksk <kaushikskalmady@gmail.com>
2017-12-10 21:15:52 +0530

    Added Euler totient function and seive of eratosthenes

--- py3/prime_utils.py
+++ py3/prime_utils.py
@@ -6,22 +6,43 @@
 # Last Modified Date: 27.11.2017
 # Last Modified By  : Kaushik S Kalmady
 
+from math import sqrt
+
+from utils import exp
+
 
 def seive(n):
     """Seive of Eratosthenes
 
     Returns a list of primes less than n
     """
-    pass
+    myseive = [False] * (n)
+
+    for i in range(2, int(sqrt(n)) + 1):
+        if not myseive[i]:
+            for j in range(i * i, n, i):
+                myseive[j] = True
+
+    return [i for i, num in enumerate(myseive) if num is False and i > 1]
+
 
 def modified_seive(n):
     """Modified Seive of Eratosthenes for calculating Euler's Totient Function
 
-    Returns a list of length n.
+    Returns a list of length n + 1.
     seive[i] is the largest prime that divides i.
 
     """
-    pass
+    myseive = [0] * (n + 1)
+    myseive[0] = myseive[1] = 1
+
+    for i in range(2, n + 1):
+        if not myseive[i]:
+            myseive[i] = i
+            for j in range(i, n + 1, i):
+                myseive[j] = i
+
+    return myseive
 
 
 def phi(n):
@@ -30,7 +51,18 @@ def phi(n):
     phi(n) = number of positive integers co-prime with n.
 
     """
-    pass
+    ans = 1
+    prime = modified_seive(n)
+
+    while(n > 1):
+        p = prime[n]
+        e = 0
+        while(n % p == 0):
+            n = n//p
+            e += 1
+        ans *= (p - 1) * exp(p, e - 1)
+
+    return ans
 
 
 if __name__ == "__main__":
