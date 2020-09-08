7d5ead90932955a9fe77db0c6640ff9e031a8b72
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2020-09-08 19:29:37 +0530

    Adds extended gcd algorithm and test

--- ts/src/gcdUtils.ts
+++ ts/src/gcdUtils.ts
@@ -1,22 +1,22 @@
-function recursiveGCD(a: number, b: number) : number {
+export function recursiveGCD(a: number, b: number): number {
     if (a < 0 || b < 0) {
-        throw new Error("Both numbers have to be >= 0");
+        throw new Error('Both numbers have to be >= 0');
     }
 
     return recursiveGCDHelper(a, b);
 }
 
-function recursiveGCDHelper(a: number, b: number) : number {
-    return a ===  0 ? b : recursiveGCDHelper(b % a,  a);
+function recursiveGCDHelper(a: number, b: number): number {
+    return a === 0 ? b : recursiveGCDHelper(b % a, a);
 }
 
-function gcd(a: number, b: number) : number {
+export function gcd(a: number, b: number): number {
     if (a < 0 || b < 0) {
-        throw new Error("Both numbers have to be >= 0");
+        throw new Error('Both numbers have to be >= 0');
     }
 
-    while (a !== 0) { 
-        let temp : number =  a
+    while (a !== 0) {
+        let temp: number = a;
         a = b % a;
         b = temp;
     }
@@ -24,4 +24,43 @@ function gcd(a: number, b: number) : number {
     return b;
 }
 
-export {recursiveGCD, gcd};
+export interface ExtendedGCDReturnValue {
+    gcd: number;
+    x: number;
+    y: number;
+}
+
+export function xgcd(a: number, b: number): ExtendedGCDReturnValue {
+    if (a < 0 || b < 0) {
+        throw new Error('Both numbers have to be >= 0');
+    }
+
+    let xprev: number = 0;
+    let x: number = 1;
+    let yprev: number = 1;
+    let y: number = 0;
+    let q: number;
+    let temp: number;
+
+    while (a !== 0) {
+        q = Math.floor(b / a);
+
+        temp = x;
+        x = xprev - q * x;
+        xprev = temp;
+
+        temp = y;
+        y = yprev - q * y;
+        yprev = temp;
+
+        temp = a;
+        a = b % a;
+        b = temp;
+    }
+
+    return {
+        gcd: b,
+        x: xprev,
+        y: yprev,
+    };
+}
--- ts/src/tests/gcdUtils.test.ts
+++ ts/src/tests/gcdUtils.test.ts
@@ -1,18 +1,68 @@
-import { recursiveGCD } from '../gcdUtils';
+import { recursiveGCD, gcd, xgcd, ExtendedGCDReturnValue } from '../gcdUtils';
 
 describe('recursiveGCD', () => {
-  it("throw error if any argument is negative",  () =>  {
-    expect(() => recursiveGCD(-1, 10)).toThrow(Error);
-    expect(() => recursiveGCD(10, -1)).toThrow(Error);
-    expect(() => recursiveGCD(-1, -1)).toThrow(Error);
-  });
+    it('throw error if any argument is negative', () => {
+        expect(() => recursiveGCD(-1, 10)).toThrow(Error);
+        expect(() => recursiveGCD(10, -1)).toThrow(Error);
+        expect(() => recursiveGCD(-1, -1)).toThrow(Error);
+    });
 
-  it("should return 1 for co-primes",  () =>  {
-    expect(recursiveGCD(7, 19)).toBe(1);
-  });
+    it('should return 1 for co-primes', () => {
+        expect(recursiveGCD(7, 19)).toBe(1);
+    });
 
-  it("should return correct gcd when exists",  () =>  {
-    expect(recursiveGCD(221,34)).toBe(17);
-    expect(recursiveGCD(34,221)).toBe(17);
-  });
+    it('should return correct gcd when exists', () => {
+        expect(recursiveGCD(221, 34)).toBe(17);
+        expect(recursiveGCD(34, 221)).toBe(17);
+    });
+
+    it('should handle the case when input is zero', () => {
+        expect(recursiveGCD(7, 0)).toBe(7);
+        expect(recursiveGCD(0, 7)).toBe(7);
+    });
+});
+
+describe('gcd', () => {
+    it('throw error if any argument is negative', () => {
+        expect(() => gcd(-1, 10)).toThrow(Error);
+        expect(() => gcd(10, -1)).toThrow(Error);
+        expect(() => gcd(-1, -1)).toThrow(Error);
+    });
+
+    it('should return 1 for co-primes', () => {
+        expect(gcd(7, 19)).toBe(1);
+    });
+
+    it('should return correct gcd when exists', () => {
+        expect(gcd(221, 34)).toBe(17);
+        expect(gcd(34, 221)).toBe(17);
+    });
+
+    it('should handle the case when input is zero', () => {
+        expect(gcd(7, 0)).toBe(7);
+        expect(gcd(0, 7)).toBe(7);
+    });
+});
+
+describe('xgcd', () => {
+    it('throw error if any argument is negative', () => {
+        expect(() => xgcd(-1, 10)).toThrow(Error);
+        expect(() => xgcd(10, -1)).toThrow(Error);
+        expect(() => xgcd(-1, -1)).toThrow(Error);
+    });
+
+    it('should return gcd 1 for co-primes', () => {
+        let xgcdReturnValue: ExtendedGCDReturnValue = xgcd(7, 19);
+        expect(xgcdReturnValue.gcd).toBe(1);
+    });
+
+    it('should return correct values when exist', () => {
+        expect(xgcd(15, 35)).toStrictEqual({ gcd: 5, x: -2, y: 1 });
+        expect(xgcd(30, 20)).toStrictEqual({ gcd: 10, x: 1, y: -1 });
+    });
+
+    it('should handle the case when input is zero', () => {
+        let xgcdReturnValue: ExtendedGCDReturnValue = xgcd(7, 0);
+        expect(xgcdReturnValue.gcd).toBe(7);
+    });
 });
